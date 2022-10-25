a25434ba96d90289376c760541e104ae6abb2a33
Cawna.sex <104955231+TiagoKl@users.noreply.github.com>
2022-10-24 22:10:23 -0500

    🩸 ⋮ ＠  𝗦𝗿 𝗖𝗮𝘄𝗻𝗮  ꒱

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(imagen1, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: 'ᴛʜᴇ ᴍʏsᴛɪᴄ - ʙᴏᴛ', body: '©𝐵𝑟𝑢𝑛𝑜𝑆𝑜𝑏𝑟𝑖𝑛𝑜', sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`, thumbnail: imagen2}}})}
+contextInfo: { externalAdReply: { title: '𝐓𝐎𝐑𝐓𝐔𝐑𝐀  𝛺̶  𝗜ꟿ𝐅Σ𝖱𝗡𝗔𝗟ᜓ̷', body: '🩸 ⋮ ＠  𝗦𝗿 𝗖𝗮𝘄𝗻𝗮  ꒱', sourceUrl: `https://www.tiktok.com/@cawna.sex`, thumbnail: imagen2}}})}
     
 return !0 }
 export default handler
