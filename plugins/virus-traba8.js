f1cc6c8666f1ee1d655d581c8cde8c16c3ae06af
Cawna.sex <104955231+TiagoKl@users.noreply.github.com>
2022-11-04 18:00:00 -0500

    ֪ ˓ 💀 ̸̷̷̸̶̤̳   𝟲5.  ֪ 𝐓̸̷𝐈̸̷͡𝗔𝗚̸̶̸̷𝐎̶̷̸  ̸̷̤̤ ̸̷̤̤ 🥃 ̸̷̤̤

--- plugins/virus-traba8.js
+++ plugins/virus-traba8.js
@@ -1,8 +1,46 @@
-import fetch from 'node-fetch'
-let handler  = async (m, { conn }) => {
-let info = await fetch('https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/trabas/traba5.txt').then(v => v.text());
-conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '🔥 *by ĐłɆ₲Ø-Ø₣₵ 🔥', 'status@broadcast')}
-handler.command = ['virus8', 'c8', 'binario8', 'traba8', 'crash8'] 
-handler.rowner = true
-handler.fail = null
-export default handler
+let handler = async (m, { conn, usedPrefix }) => { 
+ var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document'] 
+ var document = doc[Math.floor(Math.random() * doc.length)]     
+ let text = ` 
+ 𝕯̵͌͌ ❯  𝗦𝗿 𝗖𝗮𝘄𝗻𝗮🍷 ;;  死 
+  
+ 🪀NÚMERO DE MI CREADOR OFICIAL🪀 
+  
+ +51 946 352 266 
+  
+ 💡CUENTA DE TIKTOK💡 
+  
+ @Cawna.Sex 
+  
+ 👾CUENTA DE INSTAGRAM👾 
+  
+ @Cawna.Sex 
+ `.trim()    
+ let buttonMessage= { 
+ 'document': { url: `https://github.com/BrunoSobrino/TheMystic-Bot-MD` }, 
+ 'mimetype': `application/${document}`, 
+ 'fileName': `𝕯̵͌͌ ❯  𝗦𝗿 𝗖𝗮𝘄𝗻𝗮🍷 ;;  死`, 
+ 'fileLength': 99999999999999, 
+ 'pageCount': 200, 
+ 'contextInfo': { 
+ 'forwardingScore': 200, 
+ 'isForwarded': true, 
+ 'externalAdReply': { 
+ 'mediaUrl': 'https://github.com/BrunoSobrino/TheMystic-Bot-MD', 
+ 'mediaType': 2, 
+ 'previewType': 'pdf', 
+ 'title': '🌻🌟EL MEJOR BOT DE WHATSAPP 🌟🌻', 
+ 'body': wm, 
+ 'thumbnail': imagen1, 
+ 'sourceUrl': 'https://www.tiktok.com/@cawna.sex' }}, 
+ 'caption': text, 
+ 'footer': wm, 
+ 'headerType': 6 } 
+ conn.sendMessage(m.chat, buttonMessage, { quoted: m }) 
+ const data = global.owner.filter(([id, isCreator]) => id && isCreator) 
+ await conn.sendContact(m.chat, data.map(([id, name]) => [id, name]), m) 
+ } 
+ handler.help = ['owner', 'creator'] 
+ handler.tags = ['info'] 
+ handler.command = /^(owner|creator|creador|propietario)$/i 
+ export default handler
